b89184468e115d5207498e91c2c7bf36d4e6b7e8
pedro vinicius <128748644+pedrosantos-21@users.noreply.github.com>
2023-06-05 14:37:35 -0300

    create organo

---
+++ src/componentes/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner(){
+    //JSX
+    return(
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="Banner principal da página"></img>
+        </header>
+    )
+}
+
+export default Banner
